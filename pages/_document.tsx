573bf13dbc1550cc6722b468e8ec1444173de2f7
Chloe <gtalk.with.nani@gmail.com>
2025-03-03 18:13:39 +0900

    앱 컬러 및 파비콘 세팅

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,29 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
+// import { GA_TRACKING_ID } from '@/lib/gtag';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ko-KR">
+      <Head>
+        <meta name="application-name" content="타임랩" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="Black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="타임랩" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#000000" />
+        <link href="/favicon/manifest.json" rel="manifest" />
+        <link href="/favicon/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
+        <link href="/favicon/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+        <link href="/favicon/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
+        <link href="/favicon/favicon.ico" rel="shortcut icon" />
+        {/* <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${GA_TRACKING_ID}', { page_path: window.location.pathname });`,
+          }}
+        /> */}
+      </Head>
       <body>
         <Main />
         <NextScript />
