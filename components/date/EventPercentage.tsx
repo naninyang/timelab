6beaeba46362707f5b6653fb71fdf447186263b4
Chloe <gtalk.with.nani@gmail.com>
2025-03-04 18:25:12 +0900

    웹접근성

--- components/date/EventPercentage.tsx
+++ components/date/EventPercentage.tsx
@@ -181,7 +181,14 @@ export default function EventPercentage() {
         )}
         {progress !== null && (
           <div className={styles.progress}>
-            <div className={styles['progress-bar']}>
+            <div
+              className={styles['progress-bar']}
+              role="progressbar"
+              aria-label="이벤트 도달 상태"
+              aria-valuenow={progress}
+              aria-valuemin={0}
+              aria-valuemax={100}
+            >
               <div className={styles.progressing} style={{ width: `${progress}%` }} />
             </div>
             <p>{progress}% 진행</p>
